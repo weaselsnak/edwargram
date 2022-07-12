ce3c9438b0159cf4d243e0df9c111c12420ef840
Edward Boyles <edwardboyles1@gmail.com>
2022-07-12 19:55:44 +1000

    prevent default on form submit
    change some 'variables' to consts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let words = ["INFLUENCE", "BARGAINED", "ENCIRCLES", "DESPAIRED", "BELITTLES", "CAMPFIRES", "AMBERGRIS", "BARNACLES", "HEADLOCKS","FESTIVITY"]
-let scrambleds= ["LCFENUNIE", "IEAANDRBG", "SLCINRECE", "EEIDPADRS", "ELTTLESBI", "ISCPRFAEM", "GMSIERRBA", "AACLRNEBS", "DSHLKOEAC", "YTETIFSVI"]
+const words = ["INFLUENCE", "BARGAINED", "ENCIRCLES", "DESPAIRED", "BELITTLES", "CAMPFIRES", "AMBERGRIS", "BARNACLES", "HEADLOCKS","FESTIVITY"]
+const scrambleds= ["LCFENUNIE", "IEAANDRBG", "SLCINRECE", "EEIDPADRS", "ELTTLESBI", "ISCPRFAEM", "GMSIERRBA", "AACLRNEBS", "DSHLKOEAC", "YTETIFSVI"]
 
 function fillTable(word) {
-    table = document.querySelector("table")
+    const table = document.querySelector("table")
     let count = 0;
     for (let i = 0; i < table.rows.length; i++) {
         for (let j= 0; j < table.rows[i].cells.length; j++) {
@@ -12,32 +12,33 @@ function fillTable(word) {
     }
 }
 
-function checkWord() {
-    word = document.getElementById("anagrammed").value
+function checkWord(e) {
+    e.preventDefault();
+    const word = document.getElementById("anagrammed").value;
     if (word.length == 0) {
         return
     }
-    console.log(days);
     if (word.toLowerCase() === words[days].toLowerCase()) {
         alert("Very nice!")
     } else {
         alert("Very bad");
-        document.getElementById("anagrammed").value = "";
+        const inputField = document.getElementById("anagrammed")
+        inputField.select();
     }
 }
 
 function getNumberOfDays(date1, date2) {
     const oneDay = 1000 * 60 * 60 * 24;
     const diffInTime = date2.getTime() - date1.getTime();
-    const diffInDays = Math.round(diffInTime / oneDay);
+    const diffInDays = Math.floor(diffInTime / oneDay);
     return diffInDays;
 }
 
 let days;
 const wordInput = document.getElementById('anagrammed');
 const beginning = new Date('2022-07-07')
-document.addEventListener('DOMContentLoaded', function() {
-    let now = new Date()
+document.addEventListener('DOMContentLoaded', () => {
+    const now = new Date()
     days = getNumberOfDays(beginning, now);
     fillTable(scrambleds[days])
 }, false);
